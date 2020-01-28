81508bdf5dd01b1e77eeedcd25015d250d0a1937
raent25 <raent25@gmail.com>
2020-01-28 15:19:57 +0200

    class editing

--- src/modules/dropdown.js
+++ src/modules/dropdown.js
@@ -3,7 +3,7 @@ class Dropdown {
         this.$el = document.querySelector(selector)
         this.$btn = document.querySelector('#language-btn')
         this.items = options.items
-        this.$el.querySelector('.dropdown__label').innerHTML = this.items[0].label
+        this.$el.querySelector('.dropdown__label').innerHTML = `<li data-id="${this.items[0].id}"><img class="dropdown__img" src="${this.items[0].label}" alt=""></li>`
         this.$el.querySelector('.dropdown__text').innerHTML = this.items[0].text
         this.$el.addEventListener('click', event => {
             if (event.target.classList.contains('dropdown__label') || event.target.classList.contains('language-btn') || event.target.classList.contains('dropdown__text') || event.target.classList.contains('language-btn__img')) {
@@ -12,22 +12,21 @@ class Dropdown {
                 } else {
                     this.open()
                 }
-            } else if (event.target.classList.contains('dropdown__li')) {
+            } else if (event.target.tagName.toLowerCase() === 'li') {
                 this.select(event.target.dataset.id)
             }
         })
 
 
         const itemsHTML = this.items.map(i => {
-            return i.label
+            return `<li data-id="${i.id}"><img class="dropdown__img" src="${i.label}" alt=""><p>${i.text}</p></li>`
         }).join(' ')
         this.$el.querySelector('.dropdown__menu').insertAdjacentHTML('afterbegin', itemsHTML)
     }
     select(id) {
-
         const item = this.items.find(i => i.id === id)
-        this.$el.querySelector('.dropdown__label').innerHTML = item.label
-        this.$el.querySelector('dropdown__text').innerHTML = item.text
+        this.$el.querySelector('.dropdown__label').innerHTML = `<li data-id="${item.id}"><img class="dropdown__img" src="${item.label}" alt=""></li>`
+        this.$el.querySelector('.dropdown__text').textContent = item.text
         this.close()
     }
     open() {
@@ -40,20 +39,18 @@ class Dropdown {
     }
 
 }
-const dropdown = new Dropdown('#dropdown', {
+const dropdown3 = new Dropdown('#dropdown', {
     items: [{
-            label: '<li  data-id="msk"  class="dropdown__li"><img src="img/en.png" class="dropdown__img" alt="0"></li>',
-            id: 'msk',
-            text: 'EN'
-        }, {
-            label: '<li data-id="msk1" class="dropdown__li"><img src="img/ua.png"  class="dropdown__img" alt="1"></li>',
-            id: 'msk1',
-            text: 'UA'
-        }, {
-            label: '<li data-id="msk2" class="dropdown__li"><img src="img/ru.png"  class="dropdown__img" alt="2"></li>',
-            id: 'msk2',
-            text: 'RU'
-        }
-
-    ]
+        label: 'img/en.png',
+        id: 'msk',
+        text: 'EN'
+    }, {
+        label: 'img/ua.png',
+        id: 'msk1',
+        text: 'UA'
+    }, {
+        label: 'img/ru.png',
+        id: 'msk2',
+        text: 'RU'
+    }]
 })
